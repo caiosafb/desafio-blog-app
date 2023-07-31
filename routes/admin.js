cc66daa6e834bb0507bd44f2ac5076c8b452ed1d
Caio Sá <caiobarcellos16@gmail.com>
2023-07-31 18:46:28 -0300

    Criando validacoes para o formulario

--- routes/admin.js
+++ routes/admin.js
@@ -21,19 +21,39 @@ router.get("/categorias/add", (req, res) => {
 })
 
 router.post("/categorias/nova", (req, res) => {
+
+    var erros = []
+
+    if(!req.body.nome || typeof req.body.nome == undefined || req.body.nome == null){
+        erros.push({texto: "Nome inválido"})
+    }
+
+    if(!req.body.slug || typeof req.body.slug == undefined || req.body.slug == null){
+        erros.push({texto: "Slug inválido"})
+    }
+
+    if(req.body.nome.length < 2){
+        erros.push({texto: "Nome da categoria é muito pequeno"})
+    }
+
+    if(erros.length > 0){
+        res.render("admin/addcategorias", {erros: erros})
+    }else{
         const novaCategoria = {
             nome: req.body.nome,
             slug: req.body.slug
         }
     
         new Categoria(novaCategoria).save().then(() => {
-            console.log("Categoria criada com sucesso!")
+            req.flash("success_msg", "Categoria criada com sucesso!")
             res.redirect("/admin/categorias")
         }).catch((err) => {
-            console.log("Erro ao salvar categoria!")
+            req.flash("error_msg", "Houve um erro ao salvar a categoria, tente novamente!")
             res.redirect("/admin")
         })
-    })
+    
+    }
+})
 
 router.get("/teste", (req, res) => {
     res.send("Isso é um teste")
