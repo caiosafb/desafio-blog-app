8446e02cacf2a011b52eb88b2e528ae100f35131
Caio Sá <caiobarcellos16@gmail.com>
2023-07-31 18:34:16 -0300

    Configurando sessoes e middleware

--- app.js
+++ app.js
@@ -6,13 +6,30 @@
     const admin = require("./routes/admin")
     const path = require("path")
     const mongoose = require("mongoose")
+    const session = require("express-session")
+    const flash = require("connect-flash")
 
 // Configurações
-    //Body Parser
+    // Sessão
+        app.use(session({
+            secret: "cursodenode",
+            resave: true,
+            saveUninitialized: true
+        }))
+        app.use(flash())
+
+    // Middleware
+        app.use((req, res, next) => {
+            res.locals.success_msg = req.flash("success_msg")
+            res.locals.error_msg = req.flash("error_msg")
+            next()
+        })
+
+    // Body Parser
         app.use(bodyParser.urlencoded({extended: true}))
         app.use(bodyParser.json())
 
-    //Handlebars
+    // Handlebars
         app.engine('handlebars', handlebars.engine({
         defaultLayout: 'main',
         runtimeOptions: {
@@ -22,7 +39,7 @@
     }))
     app.set('view engine', 'handlebars')
 
-    //Mongoose
+    // Mongoose
         mongoose.Promise = global.Promise;
                 mongoose.connect('mongodb://127.0.0.1/blogapp', {
                     useNewUrlParser: true, 
@@ -36,7 +53,7 @@
 
     // Public 
         app.use(express.static(path.join(__dirname, "public")))
-        
+
 // Rotas
 app.use('/admin', admin)
 app.get('/posts', (req, res) => {
