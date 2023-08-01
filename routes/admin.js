f02148b7fc62f0e37f5675d2004bde6d3a4bc279
Caio Sá <caiobarcellos16@gmail.com>
2023-08-01 12:11:28 -0300

    Salvando postagens no banco de dados

--- routes/admin.js
+++ routes/admin.js
@@ -140,5 +140,34 @@ router.get("/postagens/add", (req, res) => {
     
 })
 
+router.post("/postagens/nova", (req, res) => {
+    var erros = []
+
+    if(req.body.categoria == "0"){
+       erros.push({texto: "Categoria inválida, registre um categoria"})
+    }
+
+    if(erros.length > 0){
+       res.render("admin/addpostagem", {erros: erros})
+    }else {
+       const novaPostagem = {
+           titulo: req.body.titulo,
+           descricao: req.body.descricao,
+           conteudo: req.body.conteudo,
+           categoria: req.body.categoria,
+           slug: req.body.slug
+       }
+       
+       new Postagem(novaPostagem).save().then(() => {
+           req.flash("success_msg", "Postagem criada com sucesso!")
+           res.redirect("/admin/postagens")
+       }).catch((err) => {
+           req.flash("error_msg", "Houve um erro durante o salvamento da postagem")
+           res.redirect("/admin/postagens")
+       })
+    }
+
+})
+
 
 module.exports = router
