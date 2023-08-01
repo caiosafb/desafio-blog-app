905ad4ab38a82b19b50fd05a87584647e2c9c34f
Caio Sá <caiobarcellos16@gmail.com>
2023-08-01 11:38:04 -0300

    Criacao da funcionalidade para editar categoria

--- routes/admin.js
+++ routes/admin.js
@@ -61,6 +61,55 @@ router.post("/categorias/nova", (req, res) => {
     }
 })
 
+router.get("/categorias/edit/:id", (req, res) => {
+    Categoria.findOne({_id:req.params.id}).lean().then((categoria) => {
+        res.render("admin/editcategorias", {categoria: categoria})
+    }).catch((err) => {
+        req.flash("error_msg", "Esta categoria não existe")
+        res.redirect("/admin/categorias")
+    })
+})
+
+router.post("/categorias/edit", (req, res) => {
+    let erros = []
+    
+    if(!req.body.nome || typeof req.body.nome == undefined || req.body.nome == null) {
+        erros.push({texto: "Nome inválido"})
+    }
+
+    if(!req.body.slug || typeof req.body.slug == undefined || req.body.slug == null) {
+        erros.push({texto: "Slug inválido"})
+    }
+
+    if(req.body.nome.length < 2){
+        erros.push({texto: "Nome da categoria muito pequeno"})
+    }
+
+    if(erros.length > 0){
+        res.render("admin/editcategorias", {erros: erros})
+    } else {
+
+    Categoria.findOne({_id: req.body.id}).then((categoria) => {
+
+        categoria.nome = req.body.nome
+        categoria.slug = req.body.slug
+
+        categoria.save().then(() => {
+            req.flash("success_msg", "Categoria editada com sucesso!")
+            res.redirect("/admin/categorias")
+        }).catch((err) => {
+            req.flash("error_msg", "Houve um erro ao salvar a edição da categoria")
+            res.redirect("/admin/categorias")
+        })
+
+    }).catch((err) => {
+        req.flash("error_msg", "Houve um erro ao editar a categoria")
+        res.redirect("/admin/categorias")
+    })
+    }
+})
+
+
 router.get("/teste", (req, res) => {
     res.send("Isso é um teste")
 })
