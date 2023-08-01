6a1400d2d19164675c61214414ebed5bc1e76124
Caio Sá <caiobarcellos16@gmail.com>
2023-08-01 14:41:53 -0300

    criacao da view para aba categorias

--- app.js
+++ app.js
@@ -10,6 +10,8 @@
     const flash = require("connect-flash")
     require("./models/Postagem")
     const Postagem = mongoose.model("postagens")
+    require("./models/Categoria")
+    const Categoria = mongoose.model("categorias")
 
 // Configurações
     // Sessão
@@ -81,14 +83,42 @@
         })
     })
 
+    app.get("/categorias/:slug", (req, res) => {
+        Categoria.findOne({slug: req.params.slug}).lean().then((categoria) => {
+            if(categoria){
+
+                Postagem.find({categoria: categoria._id}).lean().then((postagens) => {
+                    res.render("categorias/postagens", {postagens: postagens, categoria: categoria})
+
+                }).catch((err) => {
+                    req.flash("error_msg", "Houve um erro ao listar os posts!")
+                    res.redirect("/")
+                })
+            
+            }else{
+                req.flash("error_msg", "Esta categoria não existe")
+                res.redirect("/")
+            }
+        }).catch((err) => {
+            req.flash("error_msg", "Houve um erro interno")
+            res.redirect("/")
+        })
+    })
+  
+    app.get("/categorias", (req, res) => {
+        Categoria.find().lean().then((categorias) => {
+            res.render("categorias/index", {categorias: categorias})
+        }).catch((err) => {
+            req.flash("error_msg", "Houve um erro ao listar as categorias")
+            res.redirect("/")
+        })
+    })
+
     app.get("/404", (req, res) => {
         res.send('Erro 404!')
     })
 
-app.use('/admin', admin)
-app.get('/posts', (req, res) => {
-    res.send('lista de posts')
-})
+    app.use('/admin', admin)
 
 // Outros
 const PORT = 8081
