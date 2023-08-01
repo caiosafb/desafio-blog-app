f39ad37bbae2c0e8178bbafb4e238a310531080a
Caio Sá <caiobarcellos16@gmail.com>
2023-08-01 13:04:03 -0300

    Criacao home page

--- app.js
+++ app.js
@@ -8,6 +8,8 @@
     const mongoose = require("mongoose")
     const session = require("express-session")
     const flash = require("connect-flash")
+    require("./models/Postagem")
+    const Postagem = mongoose.model("postagens")
 
 // Configurações
     // Sessão
@@ -55,6 +57,20 @@
         app.use(express.static(path.join(__dirname, "public")))
 
 // Rotas
+
+    app.get('/', (req, res) => {
+        Postagem.find().lean().populate("categoria").sort({data: 'desc'}).then((postagens) => {
+            res.render("index", {postagens: postagens})
+        }).catch((err) => {
+            req.flash("error_msg", "Houve um erro interno")
+            res.redirect("/404")
+        })
+    })
+
+    app.get("/404", (req, res) => {
+        res.send('Erro 404!')
+    })
+
 app.use('/admin', admin)
 app.get('/posts', (req, res) => {
     res.send('lista de posts')
